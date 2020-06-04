9005fd0dd7037733499a2bf61d5aadc6bdb4a8d2
Siddhant saxena <66401356+mr-siddy@users.noreply.github.com>
2020-06-05 02:13:06 +0530

    Add files via upload

---
+++ Introduction/Basic Datatypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a ;
+    char b ;
+    float c ; 
+    double d ;
+    long e ; 
+    
+   scanf("%d %ld %c %f %lf ",&a,&e,&b,&c ,&d);
+
+   printf("%d\n%ld\n%c\n%0.3f\n%0.9lf",a,e,b,c,d);
+
+    return 0;
+}
